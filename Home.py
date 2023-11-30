a378e0a6b6cd1e0b13eb403a718a00d1bf6b9954
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-30 09:59:31 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,15 +5,11 @@ import numpy as np
 
 with st.sidebar:
 
-    st.subheader("QC type")
-    qc_type = st.selectbox(label = "QC type", options= ["Year to year", "Audit"], index = 0, label_visibility= "hidden")
+    qc_type = st.selectbox(label = "QC type", options= ["Year to year", "Audit"], index = 0)
     
-    st.subheader("Select Pathway data")
-    data1 = st.file_uploader("Select Pathway data", label_visibility= "hidden")
+    data1 = st.file_uploader("Select Pathway data")
     
     if qc_type == "Audit":
-        st.subheader("Select audit data")
-        data2 = st.file_uploader("Select audit data", label_visibility= "hidden")
+        data2 = st.file_uploader("Select audit data")
     
-    st.subheader("Performance measures")
-    perf_index = st.multiselect(label = "Select measures", options= ["IRI", "RUT"], label_visibility= "hidden")
+    perf_index = st.multiselect(label = "Select measures", options= ["IRI", "RUT"])

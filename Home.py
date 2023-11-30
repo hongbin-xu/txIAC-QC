a3abe8fde90b7f35aca72a749f8e975b5d6dce90
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-30 14:43:01 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,9 +7,25 @@ with st.sidebar:
 
     qc_type = st.selectbox(label = "QC type", options= ["Year to year", "Audit"], index = 0)
     
-    data1 = st.file_uploader("Select Pathway data")
+    data1_path = st.file_uploader("Select Pathway data")
+    data1 = pd.read_csv(data1_path)
     
     if qc_type == "Audit":
-        data2 = st.file_uploader("Select audit data")
+        data2_path = st.file_uploader("Select audit data")
+        data2 = pd.read_csv(data2_path)
     
     perf_index = st.multiselect(label = "Select measures", options= ["IRI", "RUT"])
+
+
+# IRI: Left IRI, Right IRI, Avg IRI
+
+# Rut: Left, right, shallow, deep, severe, failure
+
+# Cracks
+    # ACP
+    # CRCP
+    # JCP
+    # JRCP
+
+
+conda install -c conda-forge/label/cf202003 nodejs

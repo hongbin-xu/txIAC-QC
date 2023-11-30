d79ba538247033818e6a73a7fbb0f19d7eadafad
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-30 09:54:34 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ import numpy as np
 with st.sidebar:
 
     qc_type = st.selectbox(label = "QC type", options= ["Year to year", "Audit"], index = 0)
-    data1 = st.file_uploader("Select Pathway data", str = "QC type")
+    data1 = st.file_uploader("Select Pathway data")
     if qc_type == "Audit":
         data2 = st.file_uploader("Select audit data")
     perf_index = st.multiselect(label = "Select measures", options= ["IRI", "RUT"])

c7ca28146da5aeaae5681cdea939870b05fc8c78
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-11-30 09:45:17 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
  
-st.
 
 with st.siderbar:
+    qc_type = st.selectbox("QC type", ["Year to year", "Audit"], ["Audit"])
+    data1 = st.file_uploader("Select Pathway data")
+    if qc_type == "Audit":
+        data2 = st.file_uploader("Select audit data")
+    distress = st.selectbox("Select distress")
+
+
+

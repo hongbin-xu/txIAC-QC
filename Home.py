5006a15dd4b2ad52fe45f9096752fba8dbaa3b21
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-12-01 12:10:38 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,20 +3,33 @@ import pandas as pd
 import numpy as np
  
 
-with st.sidebar:
 
-    qc_type = st.selectbox(label = "QC type", options= ["Year to year", "Audit"], index = 0)
-    
-    data1_path = st.file_uploader("Select Pathway data")
-    data1 = pd.read_csv(data1_path)
-    
-    if qc_type == "Audit":
-        data2_path = st.file_uploader("Select audit data")
-        data2 = pd.read_csv(data2_path)
-    
-    perf_index = st.multiselect(label = "Select measures", options= ["IRI", "RUT"])
+@st.function
+def merge(data1, data2, perf_index, qctype = "Audit")
+    data_combined = data1.merge(data2, how = , by = )
 
 
+
+
+
+
+
+
+def main():
+    with st.sidebar:
+
+        qc_type = st.selectbox(label = "QC type", options= ["Year to year", "Audit"], index = 1)
+        
+        data1_path = st.file_uploader("Select Pathway data")
+        if data1_path.exists():
+            data1 = pd.read_csv(data1_path)
+        
+        if qc_type == "Audit":
+            data2_path = st.file_uploader("Select audit data")
+            if data2_path.exists():
+                data2 = pd.read_csv(data2_path)
+        
+        perf_index = st.multiselect(label = "Select measures", options= ["IRI", "RUT"])
 
 
 

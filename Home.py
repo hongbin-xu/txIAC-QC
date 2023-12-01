e30fe3c4449a474c5da5fc52de655350a6a3b0c2
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-12-01 12:13:46 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,12 +19,12 @@ def main():
         qc_type = st.selectbox(label = "QC type", options= ["Year to year", "Audit"], index = 1)
         
         data1_path = st.file_uploader("Select Pathway data")
-        if data1_path.exists():
+        if "data1_path" in globals():
             data1 = pd.read_csv(data1_path)
         
         if qc_type == "Audit":
             data2_path = st.file_uploader("Select audit data")
-            if data2_path.exists():
+            if "data2_path" in globals():
                 data2 = pd.read_csv(data2_path)
         
         perf_index = st.multiselect(label = "Select measures", options= ["IRI", "RUT"])

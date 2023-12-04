dc5e8696da7760352aec2d8cc6edbb57f2e4286e
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-12-04 16:00:54 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -104,10 +104,10 @@ def main():
                 data2_path = st.file_uploader("Select audit data")
                 if "data2_path" in globals():
                     data2 = pd.read_csv(data2_path)
-
                 
             pav_type = st.selectbox(label = "Pavement type", options = ["ACP", "CRCP", "JCP"])
             perf_indx = st.multiselect(label = "Select measures", options= perf_indx_list[pav_type].keys())
+            data = data_merge(data1 = None, data2 = None, qctype = "Audit", pavtype= "ACP", perf_indx = None)
             st.write(perf_indx)
             st.subheader("Filter")
 

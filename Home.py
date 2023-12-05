a539b8ee3d2ffaac8f8e8ed2ae98021b3d547145
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-12-05 00:08:33 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -139,9 +139,9 @@ with st.container():
             #fig.update_layout(yaxis_title='Count', yaxis2=dict(title='cdf', overlaying='y', side='right'))
             #st.plotly_chart(fig)
 
-            hist = go.Histogram(x=data["d_"+item], nbinsx=30, yaxis_title = "count")
+            hist = go.Histogram(x=data["d_"+item], nbinsx=30)
             ecdf = px.ecdf(data, x="d_"+item)
-            ecdf = go.Scatter(x=ecdf._data[0]["x"], y=ecdf._data[0]['y'], mode='lines', yaxis_title='cdf', yaxis='y2')
+            ecdf = go.Scatter(x=ecdf._data[0]["x"], y=ecdf._data[0]['y'], mode='lines',  yaxis='y2')
             fig.add_trace(hist, row=row, col=col, secondary_y = False)
             fig.add_trace(ecdf, row=row, col=col, secondary_y = True)
             #fig.update_layout(row = row, col = col, yaxis_title='Count', yaxis2=dict(title='cdf', overlaying='y', side='right'))

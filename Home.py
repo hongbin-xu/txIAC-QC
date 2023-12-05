36c8e50749d7e8fda696da5ff5c66eb225516dbb
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-12-05 10:33:37 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -124,7 +124,7 @@ with st.sidebar:
             for p in perf_indx:            
                 i = 0
                 for item in perf_indx_list[pav_type][p]:
-                    data_v1  = data_v1.loc[abs(data_v1["d_"+item])>=thresholds[i], "flag"]=1
+                    data_v1  = data_v1.loc[abs(data_v1["d_"+item])>=thresholds[i], ["flag"]]=1
                     i+=1
             data_v1 = data_v1.loc[data_v1["flag"]].reset_index(drop = True)
 

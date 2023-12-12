d4ab8b320163d2d95533072a96564e09ffd10574
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2023-12-11 22:42:39 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -99,21 +99,21 @@ def data_merge(data1 = None, data2 = None, qctype = "Audit", pavtype= "ACP", ite
         suffixes = ["_"+str(year1), "_"+str(year2)]
 
     # merging data1 and data2
-    data_v1 = data1.loc[data1["COUNTY"].isin(data2["COUNTY"]), ["SIGNED HWY AND ROADBED ID", "COUNTY", "BEGINNING DFO", "ENDING DFO"]].reset_index(drop = True)
+    data_v1 = data1.loc[data1["COUNTY"].isin(data2["COUNTY"])].reset_index(drop = True)
     data_v1["id"]= np.arange(data_v1.shape[0])
-    data_v2 = data2[["SIGNED HWY AND ROADBED ID", "COUNTY", "BEGINNING DFO", "ENDING DFO"]]
+    data_v2 = data2.copy()
     data_v2["id"] = np.arange(data_v2.shape[0])
 
-    id_match = data_v1.merge(data_v2,
-                            how ="left", 
-                            on = ["SIGNED HWY AND ROADBED ID", "COUNTY"],
-                            suffixes= suffixes)
+    id_match = data_v1[["SIGNED HWY AND ROADBED ID", "COUNTY", "BEGINNING DFO", "ENDING DFO", "id"]].merge(data_v2[["SIGNED HWY AND ROADBED ID", "COUNTY", "BEGINNING DFO", "ENDING DFO", "id"]],
+                                                                                                            how ="left", 
+                                                                                                            on = ["SIGNED HWY AND ROADBED ID", "COUNTY"],
+                                                                                                            suffixes= suffixes)
     
     id_match = id_match.loc[(abs(id_match["BEGINNING DFO"+suffixes[0]]-id_match["BEGINNING DFO"+suffixes[1]])<0.05)&(abs(id_match["ENDING DFO"+suffixes[0]]-id_match["ENDING DFO"+suffixes[1]])<0.05)]
     #data = data.merge(data2, on = ["SIGNED HWY AND ROADBED ID", "COUNTY"], suffixes= suffixes) # merge data
     #data = data.loc[(abs(data["BEGINNING DFO"+suffixes[0]]-data["BEGINNING DFO"+suffixes[1]])<0.05)&(abs(data["ENDING DFO"+suffixes[0]]-data["ENDING DFO"+suffixes[1]])<0.05)]
-    data = id_match[["id"+suffixes[0], "id"+suffixes[1]]].merge(data1, how = "left", left_on = "id"+suffixes[0], right_on = "id") # merge data
-    data = data.merge(data2, how = "left", left_on = "id"+suffixes[1], right_on = "id")
+    data = id_match[["id"+suffixes[0], "id"+suffixes[1]]].merge(data_v1, how = "left", left_on = "id"+suffixes[0], right_on = "id") # merge data
+    data = data.merge(data_v2, how = "left", left_on = "id"+suffixes[1], right_on = "id")
 
     for item in  item_list:
         if "UTIL" not in item:

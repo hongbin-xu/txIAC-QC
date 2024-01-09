ab7bc7c3325838394577efc379edfd210a8439af
hongbin-xu <36011407+hongbin-xu@users.noreply.github.com>
2024-01-09 15:23:29 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -186,7 +186,7 @@ with st.sidebar:
                 if "UTIL" not in item:
                     threvals = np.nanpercentile(abs(st.session_state["data"]["diff_"+item].values), [25, 75])
                     threvals = [threvals[0]-1.5*(threvals[1]-threvals[0]), threvals[1]+1.5*(threvals[1]-threvals[0])]
-                    threshold_temp = [st.number_input(label = "diff_"+item+"_lower", value = threvals[0]), st.number_input(label = "maxdiff_"+item+"_upper", value = threvals[1])]
+                    threshold_temp = [st.number_input(label = "diff_"+item+"_lower", value = threvals[0]), st.number_input(label = "diff_"+item+"_upper", value = threvals[1])]
                     thresholds[item] = threshold_temp
 
         st.write(thresholds)
